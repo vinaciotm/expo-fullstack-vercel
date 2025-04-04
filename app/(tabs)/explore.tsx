774966efe2a34abcb1fe7a6c6b2b49db61e3e9df
Inácio Pires <viniciusinaciopires@gmail.com>
2025-04-04 20:15:06 -0300

    simplify example

--- app/(tabs)/explore.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text } from "react-native";
-
-export default function explore() {
-  return (
-    <SafeAreaView>
-      <Text>explore</Text>
-    </SafeAreaView>
-  );
-}
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
-import { Button, SafeAreaView } from "react-native";
+import { Button, SafeAreaView, View } from "react-native";
 
 const jsonString = (json: any) => JSON.stringify(json, null, 2);
 
 export default function App() {
   async function get(url: string) {
     // GET hello
-    fetch(url + "/hello")
+    fetch(url + "/hello", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
       .then((response) => {
         // check if ok
         if (!response.ok) alert("GET !OK: " + jsonString(response.body));
@@ -15,10 +22,12 @@ export default function App() {
       .then((data) => {
         // alert data
         alert("GET DATA: " + jsonString(data));
+        console.log(url + "GET DATA: " + jsonString(data));
       })
       .catch((error) => {
         // alert error
         alert("GET ERROR: " + jsonString(error));
+        console.error(url + "GET ERROR: " + jsonString(error));
       });
   }
 
@@ -26,7 +35,11 @@ export default function App() {
     // POST hello/POST
     fetch(url + "/hello", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
       body: JSON.stringify({ fromClient: "message from client!" }),
     })
       .then((response) => {
@@ -38,10 +51,12 @@ export default function App() {
       .then((data) => {
         // alert data
         alert("POST DATA: " + jsonString(data));
+        console.log(url + "POST DATA: " + jsonString(data));
       })
       .catch((error) => {
         // alert error
         alert("POST ERROR: " + jsonString(error));
+        console.error(url + "POST ERROR: " + jsonString(error));
       });
   }
 
@@ -51,14 +66,16 @@ export default function App() {
 
   return (
     <SafeAreaView>
-      <Button title="get vercel" onPress={() => get(VERCEL_URL)} />
-      <Button title="post vercel" onPress={() => post(VERCEL_URL)} />
+      <View style={{ gap: 10, padding: 20 }}>
+        <Button title="get vercel" onPress={() => get(VERCEL_URL)} />
+        <Button title="post vercel" onPress={() => post(VERCEL_URL)} />
 
-      <Button title="get eas" onPress={() => get(EAS_URL)} />
-      <Button title="post eas" onPress={() => post(EAS_URL)} />
+        <Button title="get eas" onPress={() => get(EAS_URL)} />
+        <Button title="post eas" onPress={() => post(EAS_URL)} />
 
-      <Button title="get localhost" onPress={() => get(LOCAL_URL)} />
-      <Button title="post localhost" onPress={() => post(LOCAL_URL)} />
+        <Button title="get localhost" onPress={() => get(LOCAL_URL)} />
+        <Button title="post localhost" onPress={() => post(LOCAL_URL)} />
+      </View>
     </SafeAreaView>
   );
 }
